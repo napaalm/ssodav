044395f454ce649e8d5b70edec633bee4d0dca8a
Antonio Napolitano <nap@napaalm.xyz>
2021-02-03 22:15:34 +0100

    Add config tests

---
+++ internal/config/config_test.go
@@ -0,0 +1,32 @@
+/*
+ * config.go
+ *
+ * File di test per il package config.
+ *
+ * Copyright (c) 2021 Antonio Napolitano <nap@napaalm.xyz>
+ *
+ * This file is part of ssodav.
+ *
+ * ssodav is free software; you can redistribute it and/or modify it
+ * under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or (at
+ * your option) any later version.
+ *
+ * ssodav is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with ssodav; see the file LICENSE. If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package config
+
+import "testing"
+
+func TestAll(t *testing.T) {
+	LoadConfig("test.toml")
+	t.Log(Config)
+}
